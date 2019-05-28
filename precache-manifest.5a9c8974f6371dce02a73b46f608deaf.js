d0486400b21d5ec8b39e1e12d7880e1e4bbfab7b
yuyiRai <yuyi919606952@foxmail.com>
2019-05-28 23:50:37 +0800

    Updates

---
+++ precache-manifest.5a9c8974f6371dce02a73b46f608deaf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e5e2f2842a0ff9d07fa4279bc220bd3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1c740114ba44c566edc0",
+    "url": "./static/css/main.a2b7b0b6.chunk.css"
+  },
+  {
+    "revision": "43b9b269815703514bf8",
+    "url": "./static/js/2.48a9174b.chunk.js"
+  },
+  {
+    "revision": "1c740114ba44c566edc0",
+    "url": "./static/js/main.4ea4181d.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
